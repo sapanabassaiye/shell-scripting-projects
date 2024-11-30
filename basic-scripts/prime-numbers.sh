6b7bc5a7eefbb4b22df4cfa66998b4b5ca75d187
Sapana Bassaiye <sapnabasaiye38350@gmail.com>
2024-12-01 00:52:14 +0530

    Create prime-numbers.sh

---
+++ basic-scripts/prime-numbers.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+############################################
+#
+#Script info: Print prime numbers
+#
+############################################
+
+for num in {2..100}; do
+        is_prime=true
+        for (( i=2; i<num; i++ ))
+        do
+                if [ $(($num%$i)) -eq 0 ]
+                then
+                        is_prime=false
+                        break
+                fi
+        done
+        if $is_prime 
+        then
+                echo $num
+        fi
+done
